3ba58bb4fcb72e64ef3b5cadc84e92e56e4dc849
znuxsni98 <3945900042@qq.com>
2026-05-01 14:05:50 +0800

    添加电台和玩偶

---
+++ kubejs/assets/fib/blockstates/ru_meng.js
@@ -0,0 +1,26 @@
+{
+    "variants":{
+        "":{
+            "fib": "fib:block/ru_meng"
+        }
+    }
+}
+{
+    "variants": {
+        "facing=east":{
+            "model": "fib:block/ru_meng",
+            "y":90
+        },
+        "facing=north":{
+            "model": "fib:block/ru_meng"
+        },
+        "facing=south":{
+            "model": "fib:block/ru_meng",
+            "y": 180
+        },
+        "facing=west":{
+            "model": "fib:block/ru_meng",
+            "y": 270
+        },
+    }
+}
