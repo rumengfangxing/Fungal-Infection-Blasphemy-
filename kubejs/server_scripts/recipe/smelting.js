2f96716aa870c466d4f50a809d8650d2e3db90ad
znuxsni98 <3945900042@qq.com>
2026-05-31 08:11:52 +0800

    粗矿块批量熔炼配方

--- kubejs/server_scripts/recipe/smelting.js
+++ kubejs/server_scripts/recipe/smelting.js
@@ -8,4 +8,13 @@ ServerEvents.recipes(event => {
         Item.of('minecraft:wither_skeleton_skull'),
         'minecraft:skeleton_skull'
     )
+
+//铜块
+event.smelting('copper_block','raw_copper_block',2.1,30*20)
+//铁块
+event.smelting('iron_block','raw_iron_block',2.1,30*20)
+//锌块
+event.smelting('create:zinc_block','create:raw_zinc_block',2.1,30*20)
+//金块
+event.smelting('gold_block','raw_gold_block',3,30*20)
 });
