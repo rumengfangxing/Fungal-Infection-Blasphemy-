441a2bde3a0b849da43c540efc3edf1ee8d079a8
rumengfangxing <2085553898@qq.com>
2026-08-01 17:08:42 +0800

    阶段性提交

--- kubejs/server_scripts/kubejs_event/player_respawned.js
+++ kubejs/server_scripts/kubejs_event/player_respawned.js
@@ -1,8 +1,5 @@
 PlayerEvents.respawned(event => {
 	const { player } = event
-	
-	checkAndEquipCursedRing(player)
-	
 	if (getFungifiedStage(player) != 0) {
 		setFungifiedStage(player, 0)
 		player.sendData("kubejs:fungified_biomass_hunger", {enable: false})
